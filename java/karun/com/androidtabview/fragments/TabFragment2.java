2cc39ecd205d1344905322ca73433a7729a03f03
hata02525 <hata02525@gmail.com>
2018-03-04 13:23:56 +0530

    Custom Tabbar UI

---
+++ java/karun/com/androidtabview/fragments/TabFragment2.java
@@ -0,0 +1,18 @@
+package karun.com.androidtabview.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import karun.com.androidtabview.R;
+
+public class TabFragment2 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.tab_fragment_1, container, false);
+    }
+}
